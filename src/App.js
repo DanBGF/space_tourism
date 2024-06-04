38d785d27c41f8e35454da9f14ac047f9f2d45ab
DanBGF <oanceadan111@gmail.com>
2024-06-04 17:26:46 +0300

    added custom bootstrap and desktop background for home
    finished navigation desktop
    
    deleted logo.svg and App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import './App.css';
+import './Custom_bootstrap/custom.bootstrap.min.css';
+import logo from '../src/img/shared/logo.svg';
+
 
 function App() {
   return (
-    <div className='bg-primary'>
+    <div>
+      <header>
+        <nav className='navbar'>
+          <img src={logo} alt='logon' className='navbar-brand'/>
+          
+          <div>
+          <hr/>
+            <ul className='navbar-nav container-fluid flex-row h-100 justify-content-center'>
+            
+              <li className='navbar-item active'>
+                <button className='navbar-link h-100 bg-transparent border-0 text-white'>
+                  <span>00</span>Home
+                </button>
+              </li>
 
+              <li className='navbar-item nactive'>
+                <button className='navbar-link h-100 bg-transparent border-0 text-white'>
+                  <span>01</span>Destination
+                </button>
+              </li>
+
+              <li className='navbar-item nactive'>
+                <button className='navbar-link h-100 bg-transparent border-0 text-white'>
+                  <span>02</span>Crew
+                </button>
+              </li>
+
+              <li className='navbar-item nactive'>
+                <button className='navbar-link h-100 bg-transparent border-0 text-white'>
+                  <span>03</span>Technology
+                </button>
+              </li>
+              </ul>
+            </div>
+          </nav>
+      </header>
+      
     </div>
   );
 }
